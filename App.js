e3cb9eb1f087b8e1e095096009c6f57e8148bcdf
monikasingh95 <78414329+monikasingh95@users.noreply.github.com>
2022-03-06 11:28:11 +0530

    Added image, about, interest

--- App.js
+++ App.js
@@ -8,9 +8,9 @@ export default function App() {
     return (
         <div>
             <About />
-            <Info />
-            <Interests />
-            <Footer />
+            {/*<Info />*/}
+            {/*<Interests />*/}
+            {/*<Footer />*/}
         </div>
     )
 }
--- components/About.js
+++ components/About.js
@@ -3,11 +3,53 @@ import reactImage from "../images/picture.jpg";
 
 export default function About () {
     return (
-        <div>
-            <img src={reactImage}
-                 alt="React Image"
-                 className="logo"
-            />
+        <div className="outerBox">
+            <div className="profilePic"></div>
+
+            <div className="rectangle91"></div>
+
+            <div className="button1">
+                <p className="linkedIn">LinkedIn</p>
+            </div>
+
+            <div className="button2">
+                <p className="email">Email</p>
+            </div>
+
+            <div className="monika-web">
+                <p>Monika's Website</p>
+            </div>
+
+            <div className="anne-gracia"></div>
+
+            <div className="react">
+                <p>Currently Learning React</p>
+            </div>
+
+            <div className="full-name">
+                <p>Monika Singh</p>
+            </div>
+
+            <div className="interest">
+                <h2>Interests</h2>
+            </div>
+
+            <div className="about">
+                <h2>About</h2>
+            </div>
+
+            <div className="my-interest">
+                <p>Reader | Badminton | Rubik's Cube | Bollywood dance | Series Buff | Coffee fanatic</p>
+            </div>
+
+            <div className="about-me">
+                <p>This is about me</p>
+            </div>
+
+            <div className="social-icon-background"></div>
+
+            <div className="github-icon"></div>
+
         </div>
     )
 }
